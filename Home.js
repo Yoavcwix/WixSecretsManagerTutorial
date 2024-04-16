4d0ba1469c9aa4142945d91d49c59a0660fc6b6d
Yoav C <33702102+Yoavcwix@users.noreply.github.com>
2024-04-16 21:04:24 +0300

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,28 @@
+/* === README ===
+This tutorial demonstrates how to work with wix-secrets-backend.v2, along with wix-fetch, wix-auth 
+and wix-web-module to securely retrieve data from a 3rd party API.  
+
+The tutorial contains code in two files: 
+- HOME (this page) - triggers the call to the backend file, and receives the data to be displayed to the site visitor.
+- BACKEND/GETDATA.WEB.JS - securely retrieves a key stored in the Secrets Manager and applies it to the third party API URL as an authentification key.
+
+To set up this tutorial, watch the video tutoral here: https://www.youtube.com/watch?v=93E7Ei5gPNM 
+or follow these steps:
+1. Create an API key on the 3rd party API of your choice.
+2. Store the API key in the Secrets Manager.
+3. Modify the code in the GETDATA.WEB.JS file to retrieve the secret's value, and call the API. Test the API call using the run functions button. 
+4. Click the button on this page and view the data retrieved in the console.
+
+For more info visit https://www.wix.com/velo/reference/wix-secrets-backend-v2
+*/
+
+import { getData } from 'backend/getData.web'
+
+$w.onReady(function () {
+
+    $w("#btnGetData").onClick(async () => {
+        const data = await getData()
+        console.log(data)
+    }); 
+
+}); //onReady Closed
